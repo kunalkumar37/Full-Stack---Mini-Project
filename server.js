323c8ed77ab18e8f8c777cca37ef6fab9dff50ed
Kunal Kumar <61112064+kunalkumar37@users.noreply.github.com>
2023-10-13 23:09:55 +0530

    Backend

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+// const express = require('express')
+const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.send('server is ready');
+
+// });
+
+//get a list of 5 jokes
+app.get('/api/jokes',(req,res)=>{
+    const jokes=[
+        {
+        id:1,
+        title:'A joke',
+        content:'This is a joke'
+        },
+
+        {
+            id:2,
+            title:'Another Joke',
+            content:'This is another joke'
+
+        },
+        {
+            id:3,
+            title:'A third joke',
+            content:'This is another joke'
+
+        },
+        {
+            id:4,
+            title:'A fourth joke',
+            content:'This is fourth joke'
+
+        },
+        {
+            id:5,
+            title:' A fifth joke',
+            content:'this is the fifth joke'
+
+        }
+    ];
+
+    res.send(jokes);
+
+});
+
+const port=process.env.PORT||5000;
+
+app.listen(port,()=>{
+    console.log(`server at http://localhost:${port}`);
+
+});
+
